befa2c3850d7d3eed0d3a04b602f263fae81ef0b
Veys Akhmedoff <vahmedov@gmail.com>
2026-07-27 21:11:58 +0300

    0.001

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Space Mono"', 'monospace'],
+        serif: ['"Space Mono"', 'monospace'],
+        mono: ['"Space Mono"', 'monospace'],
+        watermark: ['"Anton SC"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
